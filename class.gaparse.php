163739a0934af4fb5b53b9b6a5b9ad76a71de6d8
gallynero <gallynero@gmail.com>
2015-06-24 11:43:08 +0200

    Fix fatal error
    Fix PHP Fatal error:  Cannot re-assign auto-global variable _COOKIE

--- class.gaparse.php
+++ class.gaparse.php
@@ -15,16 +15,13 @@
 // License: LGPL
 //
 ////////////////////////////////////////////////////
-
 class GA_Parse
 {
-
   var $campaign_source;    		// Campaign Source
   var $campaign_name;  			// Campaign Name
   var $campaign_medium;    		// Campaign Medium
   var $campaign_content;   		// Campaign Content
   var $campaign_term;      		// Campaign Term
-
   var $first_visit;      		// Date of first visit
   var $previous_visit;			// Date of previous visit
   var $current_visit_started;	// Current visit started at
@@ -32,28 +29,23 @@ class GA_Parse
   var $pages_viewed;			// Pages viewed in current session
   
   
-  function __construct($_COOKIE) {
+  function __construct($COOKIE) {
 	   // If we have the cookies we can go ahead and parse them.
-	   if (isset($_COOKIE["__utma"]) and isset($_COOKIE["__utmz"])) {
-	       $this->ParseCookies();      
+	   if (isset($COOKIE["__utma"]) and isset($COOKIE["__utmz"])) {
+	       $this->ParseCookies($COOKIE);      
        }
       
   }
-
-  function ParseCookies(){
-
+  function ParseCookies($COOKIE){
   // Parse __utmz cookie
-  list($domain_hash,$timestamp, $session_number, $campaign_numer, $campaign_data) = preg_split('[\.]', $_COOKIE["__utmz"],5);
-
+  list($domain_hash,$timestamp, $session_number, $campaign_numer, $campaign_data) = preg_split('[\.]', $COOKIE["__utmz"],5);
   // Parse the campaign data
   $campaign_data = parse_str(strtr($campaign_data, "|", "&"));
-
   $this->campaign_source = $utmcsr;
   $this->campaign_name = $utmccn;
   $this->campaign_medium = $utmcmd;
   if (isset($utmctr)) $this->campaign_term = $utmctr;
   if (isset($utmcct)) $this->campaign_content = $utmcct;
-
   // You should tag you campaigns manually to have a full view
   // of your adwords campaigns data. 
   // The same happens with Urchin, tag manually to have your campaign data parsed properly.
@@ -65,31 +57,20 @@ class GA_Parse
     $this->campaign_content = "";
     $this->campaign_term = $utmctr;
   }
-
   // Parse the __utma Cookie
-  list($domain_hash,$random_id,$time_initial_visit,$time_beginning_previous_visit,$time_beginning_current_visit,$session_counter) = preg_split('[\.]', $_COOKIE["__utma"]);
-
+  list($domain_hash,$random_id,$time_initial_visit,$time_beginning_previous_visit,$time_beginning_current_visit,$session_counter) = preg_split('[\.]', $COOKIE["__utma"]);
   $this->first_visit = new \DateTime();
   $this->first_visit->setTimestamp($time_initial_visit);
-
   $this->previous_visit = new \DateTime();
   $this->previous_visit->setTimestamp($time_beginning_previous_visit);
-
   $this->current_visit_started = new \DateTime();
   $this->current_visit_started->setTimestamp($time_beginning_current_visit);
-
   $this->times_visited = $session_counter;
-
   // Parse the __utmb Cookie
-
-  list($domain_hash,$pages_viewed,$garbage,$time_beginning_current_session) = preg_split('[\.]', $_COOKIE["__utmb"]);
+  list($domain_hash,$pages_viewed,$garbage,$time_beginning_current_session) = preg_split('[\.]', $COOKIE["__utmb"]);
   $this->pages_viewed = $pages_viewed;
-
-
  // End ParseCookies
  }  
-
 // End GA_Parse
 }
-
 ?>
